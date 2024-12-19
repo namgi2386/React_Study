ba29671af40ad984888b8fec80012a7149cecd5b
imnamgi <happynj2697@gmail.com>
2024-12-19 15:33:27 +0900

    node project 생성

---
+++ section03/src/index.js
@@ -0,0 +1,9 @@
+// import nnn from "./math.js"
+// import nnn , { add , sub } from "./math.js";
+// console.log(add(1,3));
+// console.log(sub(1,2));
+// console.log(nnn(3,4));
+
+import randomColor from "randomcolor";
+const color = randomColor();
+console.log(color);
